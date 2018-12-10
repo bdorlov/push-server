dd4be87500432a132320cfba1b9321f38274367c
Boris Orlov <borisorlov@Mac-mini-Boris.local>
2018-12-11 02:12:04 +0300

    update

--- index.js
+++ index.js
@@ -3,13 +3,37 @@ const fs = require('fs');
 const url = require('url');
 const gcm = require('node-gcm');
 
+let db = [];
+if (fs.existsSync('db')) {
+  try {
+    db = JSON.parse(fs.readFileSync('db'));
+  } catch(e) {
+    console.log('Error in loading:', e);
+  }
+}
+
+function addPushNotificationData(data) {
+  for (item of db) {
+    if (item.token === data.token) {
+      return;
+    }
+  }
+  db.push(data);
+  fs.writeFile('db', JSON.stringify(db), (err) => {
+    if (err) {
+      console.log('Error in addPushNotificationData', err);
+    }
+  });
+}
+
+
 const appToken = fs.readFileSync('token').toString().trim();
 const sender = new gcm.Sender(appToken);
 
-function sendPushNotification(token, text) {
+function sendPushNotification(token, text, header = "Flowers and gifts") {
   const message = new gcm.Message({
     notification: {
-      title: "Flowers and gifts",
+      title: header,
       icon: "ic_launcher",
       body: text
     }
@@ -25,7 +49,7 @@ function sendPushNotification(token, text) {
   });
 }
 
-let database
+/*let database
 try {
   database = fs.readFileSync('database.json');
   database = JSON.parse(database);
@@ -35,12 +59,15 @@ try {
   } else {
     console.log('error = ', Object.keys(e));
   }
-}
+}*/
 
-const template = fs.readFileSync('index.html');
 
-function createDeviceItemHTML(device) {
-  return `<li>${device}</li>`
+
+function createDeviceItemHTML(data) {
+  return `<div class="device" data-token="${data.token}">
+    <div class="device__token">Token: ${data.token}</div>
+    <div class="device__os">OS: ${data.os} - ${data.version}</div>
+  </div>`.replace(/[\t\n]/g, '');
 }
 
 function createDeviceListHTML(list) {
@@ -48,6 +75,7 @@ function createDeviceListHTML(list) {
   for (let device of list) {
     html += createDeviceItemHTML(device);
   }
+  console.log('HTML = ', html)
   return html;
 }
 
@@ -67,15 +95,16 @@ const server = http.createServer((req, res) => {
   const path = URL.pathname;
   console.log('URL = ', URL);
   if (path === '/') {
+    const template = fs.readFileSync('index.html').toString();
     const context = {
-      list: createDeviceListHTML(database)
+      list: createDeviceListHTML(db)
     };
-    const html = render(template);
+    const html = render(template, context);
     res.writeHead(200, {
       'Content-Type': 'text/html'
     });
     res.end(html);
-  } else if (path === '/new-token') {
+  /*} else if (path === '/new-token') {
     const token = URL.query.token;
     console.log('token = ', token);
     message = 'hello from node.js!';
@@ -83,9 +112,40 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       'Content-Type': 'text/plain'
     });
+    res.end('ok');*/
+  } else if (path === '/push-notification-data') {
+    const token = URL.query.token;
+    const os = URL.query.os;
+    const version = URL.query.version;
+    console.log(token, os, version);
+    addPushNotificationData({token, os, version});
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
     res.end('ok');
-  } else if (path === 'send') {
-    res.end('');
+  } else if (path === '/send') {
+    if (req.method === 'POST') {
+      let body = '';
+      req.on('data', function(data) {
+        body += data;
+      });
+      req.on('end', function() {
+        const data = JSON.parse(body.toString());
+        const token = data.token;
+        const header = data.header;
+        const text = data.body;
+        console.log('token = ', token);
+        console.log('header = ', header);
+        console.log('body = ', text);
+        sendPushNotification(token, text, header);
+      });
+    } else {
+      res.writeHead(405, {
+        'Content-Type': 'text/plain'
+      });
+      return res.end('Method not allowed!');
+    }
+    res.end('ok');
   } else {
     res.writeHead(404, {
       'Content-Type': 'text/html'
